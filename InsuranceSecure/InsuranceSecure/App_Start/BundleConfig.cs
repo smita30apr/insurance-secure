76e8fc77691b90e5c42a8a87e2c5b00a21930a73
Smita Srivastava <smita30apr@gmail.com>
2016-07-26 15:41:44 +1000

    Initial commit

---
+++ InsuranceSecure/InsuranceSecure/App_Start/BundleConfig.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace InsuranceSecure
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles, string basePath)
+        {
+            var styleBundles =
+                from bundleDir in Directory.EnumerateDirectories(Path.Combine(basePath, "css"), "*")
+                let name = Path.GetFileName(bundleDir)
+                select
+                    new StyleBundle("~/css/" + name + ".min.css")
+                    .IncludeDirectory("~/css/" + name + "/", "*.css", true);
+
+            var scriptBundles =
+                from bundleDir in Directory.EnumerateDirectories(Path.Combine(basePath, "js"), "*")
+                let name = Path.GetFileName(bundleDir)
+                select
+                    new ScriptBundle($"~/js/{name}.min.js")
+                    .IncludeDirectory($"~/js/{name}/", "*.js", true);
+
+            var allBundles = styleBundles.Concat(scriptBundles);
+
+            foreach (var bundle in allBundles)
+            {
+                bundles.Add(bundle);
+            }
+        }
+    }
+}
---
+++ InsuranceSecure/InsuranceSecure/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InsuranceSecure.Startup))]
+namespace InsuranceSecure
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
